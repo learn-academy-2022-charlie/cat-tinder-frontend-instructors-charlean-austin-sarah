b36570512ca0db1287a211fbb9a9882e56660330
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-06-28 14:34:51 -0700

    testing with jest and enzyme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 import catLogo from '../assets/cat-logo.png'
+import { NavLink } from 'react-router-dom'
+
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <img src={catLogo} alt="logo for Cat Tinder" className="cat-logo" />
+        <NavLink to='/'>
+          <img src={catLogo} alt="logo for Cat Tinder" className="cat-logo" />
+        </NavLink>
+        <NavLink to='/catindex'>
+          <p>See all the cats</p>
+        </NavLink>
+        <NavLink to='/catnew'>
+          <p>Add a cat</p>
+        </NavLink>
       </header>
     )
   }
